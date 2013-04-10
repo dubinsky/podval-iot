e13caaa2bf6f4ca9b8c8262ba5030a7cc7c4c2f7
Leonid Dubinsky <dub@podval.org>
2013-04-10 00:39:20 -0400

    Started on the Ads1x15 (ADC) support.

--- src/main/scala/org/podval/iot/app/Sensor.scala
+++ src/main/scala/org/podval/iot/app/Sensor.scala
@@ -28,11 +28,11 @@ object Sensor {
     val pi = new RaspberryPi
     val bus = pi.i2c
     val sht21 = new Sht21(bus)
-    val sht21Display = new SevenSegment(bus, 1)
-    val mpl115a2 = new Mpl115a2(bus)
-    val mpl115a2Display = new SevenSegment(bus, 0)
+    val sht21Display = new SevenSegment(bus, 0)
+//    val mpl115a2 = new Mpl115a2(bus)
+//    val mpl115a2Display = new SevenSegment(bus, 1)
 
-//    val timeDisplay = new SevenSegment(pi.i2c, 1)
+    val timeDisplay = new SevenSegment(pi.i2c, 2)
 
     while (true) {
       val temperature = math.max(0, math.round(sht21.temperature))
@@ -46,19 +46,19 @@ object Sensor {
 
       sht21Display.update
 
-      val temperature2 = math.max(0, math.round(mpl115a2.temperature))
-      mpl115a2Display.left = temperature2
-      mpl115a2Display.leftDot = true
+//      val temperature2 = math.max(0, math.round(mpl115a2.temperature))
+//      mpl115a2Display.left = temperature2
+//      mpl115a2Display.leftDot = true
 
-      mpl115a2Display.update
+//      mpl115a2Display.update
 
 
-//      val date = new Date
-//
-//      timeDisplay.left = date.getHours
-//      timeDisplay.right = date.getMinutes
-//      timeDisplay.colon = (date.getSeconds % 2) == 0
-//      timeDisplay.update
+      val date = new Date
+
+      timeDisplay.left = date.getHours
+      timeDisplay.right = date.getMinutes
+      timeDisplay.colon = (date.getSeconds % 2) == 0
+      timeDisplay.update
 
       Thread.sleep(1000)
     }
